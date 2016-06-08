33d13887fcf2e8862454710b6778366e277ea172
Shireesh Jayashetty <shireeshj@gmail.com>
2016-06-08 09:05:44 +0530

    Update test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,9 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 
+require 'simplecov'
+SimpleCov.start
+
 require 'ariadne'
 require 'minitest/autorun'
 require 'ariadne/testing'
