1f90f041e03ab3b2c88270f632a961e4460afac4
Swapnil Shende <swaps19@users.noreply.github.com>
2016-11-17 17:02:06 +0530

    documentation added (#3)
    bundler upgraded
    
    json upgraded to 2.0.2

--- lib/ariadne/ariadne_util.rb
+++ lib/ariadne/ariadne_util.rb
@@ -4,7 +4,17 @@ require 'time'
 require 'redis'
 require 'redis/connection/hiredis'
 
+# This module acts as an iterface to the redis db
+# It will store the data that is being passed to the redis database.
+# In addition it will also store Timestamp info as its purpose is
+#   to serve as a plugin for the main timer in PAC exams.
+# This module closely works with the rails app and uses its
+#   environment params.
 module Ariadne
+  # Get the current application name for which
+  #   the data needs to be stored.
+  # This app_name will serve the purpose of generating unique keys
+  #   for each different app.
   def self.get_app_name(app_name: nil)
     app_name ||= ( ENV['APP_NAME'] || '' )
   rescue StandardError => e
@@ -13,6 +23,9 @@ module Ariadne
 
   DataUtil.init_redis_cli(redis_obj: Redis.new(url: ENV['REDIS_URL']))
 
+  # => options: a hash that will accept the data to be inserted
+  #     this hash must contain the `id` parameter
+  # => app_name: custom app_name if not stored in the ENV of rails app
   def self.insert_data(options: {}, app_name: nil)
     raise 'Please specify data to be inserted for Ariadne.insert_data method!' if options.empty?
     raise 'Please specify id to be passed for Ariadne.insert_data method!' if options['id'].nil? || options['id'].size == 0
@@ -22,6 +35,8 @@ module Ariadne
     e
   end
 
+  # => id: id for which the data is needed to be retirvied from db
+  # This method will return data as a JSON string
   def self.get_data(id: nil, app_name: nil)
     DataUtil.get_data_from_redis(id: id, app_name: get_app_name(app_name: app_name))
   rescue StandardError => e
@@ -29,6 +44,13 @@ module Ariadne
     e
   end
 
+  # This method will return the data only if the time difference is more than
+  #   the acceptable difference.
+  # The acceptable time can be specified in the data itself with `delay_interval`
+  #   as a key in the data hash while inserting.
+  # If it is not found in the data, the default acceptable time will be considered.
+  # Default value for it is 30 seconds
+  # Method will return a JSON data
   def self.get_data_with_time_difference(id: nil, app_name: nil)
     redis_data = get_data(id: id, app_name: app_name)
     default_time_diff_threshold = 30
